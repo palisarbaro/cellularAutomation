963b0537e2b44ec6b70bc60d6243f624b2164b5c
Kolesin Andrei <kolesinadrei1910@gmail.com>
2018-02-25 14:14:16 +0300

    отформатирован код в gameOfLife

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,4 +1,6 @@
 import cellularAutomaton
+
+
 class gameOfLife(cellularAutomaton.cellularAutomaton):
     def state0(self, x, y):
         count = self.countAround(x, y, 1)
@@ -6,16 +8,16 @@ class gameOfLife(cellularAutomaton.cellularAutomaton):
             return 1
         else:
             return 0
-    def state1(self,x,y):
-        count = self.countAround(x,y,1)
-        if count==2 or count==3:
+
+    def state1(self, x, y):
+        count = self.countAround(x, y, 1)
+        if count == 2 or count == 3:
             return 1
         else:
             return 0
 
     def __init__(self, sizeX, sizeY,
                  looping=True):
-        super().__init__(sizeX,sizeY,2,0,looping)
+        super().__init__(sizeX, sizeY, 2, 0, looping)
         self.transitionFunctionList[0] = self.state0
         self.transitionFunctionList[1] = self.state1
-
